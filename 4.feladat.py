243881b89374ac99d2d15e9d7634fe2401b60849
Danó Tibor <timovili6@gmail.com>
2021-05-12 00:24:19 +0200

    4 feladat

--- 4.feladat.py
+++ 4.feladat.py
@@ -12,9 +12,14 @@ for i in range(0, len(lista)):
         for k in range(j,len(lista[i])+1):
             if lista[i][j:k] == "":
                 k += 1
-            print(i,j,k)
+            elif len(lista[i][j:k]) > 3 and lista[i][j:k] == lista[i][j:k][::-1]:
+                lista2.append('{} {}'.format(lista[i][j:k],i))
+print(lista2)
 
-
+listasorted = sorted(lista2)
+vegso = sorted(lista,key=len)
+for i in range(len(vegso)):
+    print(vegso[i])
 
 
 
