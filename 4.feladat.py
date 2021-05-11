92aac0f1fdf76a8cf1d8300616be4bf7306c5e54
Danó Tibor <timovili6@gmail.com>
2021-05-12 00:26:01 +0200

    4 feladat

--- 4.feladat.py
+++ 4.feladat.py
@@ -8,7 +8,7 @@ while len(lista) != 16:
 print(lista)
 lista2 = []
 for i in range(0, len(lista)):
-    for j in range(len(lista[i]+1)):
+    for j in range(len(lista[i])+1):
         for k in range(j,len(lista[i])+1):
             if lista[i][j:k] == "":
                 k += 1
