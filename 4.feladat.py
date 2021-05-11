7f459c99e337eb43bd4db5cee32e9bf7a1799673
Danó Tibor <timovili6@gmail.com>
2021-05-12 00:18:21 +0200

    4 feladat

--- 4.feladat.py
+++ 4.feladat.py
@@ -1,10 +1,20 @@
-def ugynaz():
-    osszefuzott = ""
-    szoveg = input("irj ide valamit: ")
-    while len(szoveg) >= 16 or szoveg == "":
-        osszefuzott += szoveg
+lista =[]
+while len(lista) != 16:
+    szo = input("Adj meg egy szót: ")
+    if szo == "":
+        break
+    else:
+        lista.append(szo)
+print(lista)
+lista2 = []
+for i in range(0, len(lista)):
+    for j in range(len(lista[i]+1)):
+        for k in range(j,len(lista[i])+1):
+            if lista[i][j:k] == "":
+                k += 1
+            print(i,j,k)
 
 
 
 
-ugynaz()
+
